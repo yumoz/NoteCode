2b8b04dd9440d24b131fa8b50ab41ece1de6b85b
yumoz <zealouszxl@163.com>
2020-12-09 23:31:09 +0800

    add sword to offer
    Signed-off-by: yumoz <zealouszxl@163.com>

---
+++ SwordToOffer/Test56_NumberApperanceInArray/test56.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<windows.h>
+
+int singleNumber(int* nums, int numsSize){
+	int result = 0;
+	for (int i = 0; i < 32; i++){//遍历32位
+		int temp = 0;//记录所有数字第i位的和
+		for (int j = 0; j < numsSize; j++){
+			temp += (nums[j] >> i) & 1;//求第i位之和
+		}
+		if (temp % 3){
+			result += 1 << i;//置1
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	int Array[] = {  2, 3, 2, 2 };
+	int len = sizeof(Array) / sizeof(Array[0]);
+
+	int ret = singleNumber(Array, len);
+	printf("只出现一次的数是：%d\n", ret);
+	system("pause");
+	return 0;
+}
